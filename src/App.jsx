e9bde9bcdb1cdbccdcb8f5a83f7c8033e08436b5
ndikrp <andhikarizky36@gmail.com>
2024-05-03 18:39:46 +0700

    /cars page for search and filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
-
+import Cars from "./pages/Cars.jsx";
 
 function App() {
-  return <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cars" element={<Cars />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Cars.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Cars() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Cars;
